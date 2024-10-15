1949dfac6b8e373dc900db4970a6dd45d38a3285
aishSolkar02 <aishwarya.solkar55@gmail.com>
2024-10-15 11:10:53 +0530

    15/10/2024

--- superpet/products/urls.py
+++ superpet/products/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.products,name='products'),
+    path('',views.ProductListView.as_view(),name='products'),
     path('royal-Canin/',views.royalCanin,name="royalCanin"),
-    path('drools/',views.drools,name="drool")
+    path('drools/',views.drools,name="drool"),
+    path('search/',views.search,name="search"),
+    path('<int:pk>/',views.ProductDetailView.as_view(),name="product-detail")
+
 ]
--- superpet/products/views.py
+++ superpet/products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Product
+from django.views.generic import ListView,DetailView
 
 # Create your views here.
 
@@ -8,8 +9,24 @@ def products(request):
 
 def royalCanin(request):
     products=Product.customManager.royalCanin()
+
     return render(request,"products.html",{"products":products})
 
 def drools(request):
     products=Product.customManager.drools()
     return render(request,"products.html",{"products":products})
+
+def search(request):
+    keyword=request.GET.get("keyword")
+    products=Product.customManager.all().filter(product_name__icontains=keyword)
+    return render(request,"products.html",{"products":products})
+
+class ProductListView(ListView):
+    model=Product
+
+class ProductDetailView(DetailView):
+    model=Product
+    template_name="products/productdetail.html"
+
+
+
