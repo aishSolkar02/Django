79dd369b4fc8b87ba9ab72e15695d1cc1f510b3e
aishSolkar02 <aishwarya.solkar55@gmail.com>
2024-10-23 10:57:45 +0530

    23/10/2024

--- superpet/superpet/views.py
+++ superpet/superpet/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponseRedirect
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
     return render(request,"index.html")
@@ -12,5 +13,16 @@ def about(request):
 def login(request):
     return render(request,"login.html")
 
+#=========================Register Page ==========================
+
 def register(request):
-    return render(request,"register.html")
+    if request.method=="GET":
+        form=UserCreationForm()
+        return render(request,"register.html",{"form":form})
+    else:
+        submited_form=UserCreationForm(request.POST)
+        if submited_form.is_valid():
+            submited_form.save()
+            return HttpResponseRedirect("/login")
+
+
