d95545255dfc5d7ed3ec59fb045de6209d2220de
aishSolkar02 <aishwarya.solkar55@gmail.com>
2024-10-29 11:02:02 +0530

    29/10/24

--- superpet/static/js/script.js
+++ superpet/static/js/script.js
@@ -1,7 +1,17 @@
+// for add to card
+
+// function updateQuantity(operation,productId)
+// {
+
+//    const inputBox= document.getElementById("quantity"+productId);
+//    inputBox.value=parseInt(inputBox.value)+operation;
+
+// }
+
 function updateQuantity(operation,productId)
 {
-
-   const inputBox= document.getElementById("quantity"+productId);
+   const inputBox=document.getElementById("quantity"+productId);
    inputBox.value=parseInt(inputBox.value)+operation;
-
 }
+
+
