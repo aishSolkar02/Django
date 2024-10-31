4182c178899b603759ec48d2bbecd57119d22ebb
aishSolkar02 <aishwarya.solkar55@gmail.com>
2024-10-31 11:09:47 +0530

    cart 31/10/2024

--- superpet/cart/views.py
+++ superpet/cart/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render,HttpResponseRedirect
+from .models import Cart,CartItem
+from products.models import Product
 
 # Create your views here.
 
@@ -6,7 +8,22 @@ def add_to_cart(request,productId):
     print("****************",productId,"***************")
     print(request.user)
     currentUser=request.user
+    cart,created=Cart.objects.get_or_create(user=currentUser)
+    cartitem,created=CartItem.objects.get_or_create(cart=cart,products=Product.customManager.get(id=productId)) #cart from above cart variable
+    quantity=int(request.GET.get("quantity"))
+    if created:
+        cartitem.quantity=quantity
+    else:
+        cartitem.quantity=cartitem.quantity+quantity
+    cartitem.save()
+
     return HttpResponseRedirect("/products")
 
 def show_cart(request):
-    return render(request,"cart.html")
+    currentUser=request.user
+    cart=Cart.objects.get(user=currentUser)
+    cartitems=cart.cartitem_set.all()
+    total=0
+    for cartitem in cartitems:
+        total+=cartitem.quantity*cartitem.products.product_price
+    return render(request,"cart.html",{"cartitems":cartitems,"total":total})
